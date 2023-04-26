0b8c7ae1d5a60189c0910dc9869c8e0d62848d8e
Андрей Анатольев <a09012018@mail.ru>
2023-04-26 18:58:14 +0500

    Q

--- brain_games/games/games_even.py
+++ brain_games/games/games_even.py
@@ -1,14 +1,14 @@
 from random import randint
 
 
-def games_even(user_name):
+def games_even_question():
     n = randint(1, 99)
     print('Question:', n)
     answer_user = ''
     while answer_user == '':
         print('Your answer: ', end='')
         answer_user = input()
-        answer_random = 'no'
+        answer_correct = 'no'
         if n % 2 == 0:
-            answer_random = 'yes'
-    return answer_random, answer_user
+            answer_correct = 'yes'
+    return answer_correct, answer_user
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
 
-import brain_games.games_even
-import brain_games.greeting
-import brain_games.wrong_answer
-import brain_games.rules_of_even
-import brain_games.congrat
+from logic_of_games import logic_games
 
 
 def main():
-    user_name = brain_games.greeting.greeting()
-    brain_games.rules_of_even.ruls_of_games_even()
-    count = 0
-    for _ in range(3):
-        answer_random, answer_user = brain_games.games_even.games_even(user_name)
-        if answer_random == answer_user:
-            count += 1
-            print('Correct!')
-        else:
-            brain_games.wrong_answer.wrong_answer(user_name, answer_user, answer_random)
-            break
-    if count == 3:
-        brain_games.congrat.congratulation(user_name)
+    logic_games()
 
 
 if __name__ == '__main__':
