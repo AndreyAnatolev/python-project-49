462322b3e64f19035ce83b8dd085c6a73eb1868c
Андрей Анатольев <a09012018@mail.ru>
2023-04-27 20:14:58 +0500

    New modul

---
+++ brain_games/logic_of_games.py
@@ -0,0 +1,23 @@
+
+import prompt
+
+
+def logic_games(game_variants):
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print(game_variants.rules_of_game)
+
+    count = 0
+    for _ in range(3):
+        answer_correct, answer_user = game_variants.games_even()
+        if answer_correct == answer_user:
+            count += 1
+            print('Correct!')
+        else:
+            print(f"'{answer_user}' is wrong answer ;(. "
+                  f"Correct answer was '{answer_correct}'. "
+                  f"Let's try again, {user_name}!")
+            break
+    if count == 3:
+        print(f'Congratulations, {user_name}!')
