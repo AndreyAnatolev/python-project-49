bd4ae4818291cf1e3344715e231dcec3070c3a1b
Андрей Анатольев <a09012018@mail.ru>
2023-04-30 16:40:45 +0500

    New script

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games import logic_of_games
+from brain_games.games import game_prime
+
+
+def main():
+    logic_of_games.logic_games(game_prime)
+
+
+if __name__ == '__main__':
+    main()
