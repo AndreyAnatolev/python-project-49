cc739d14dd9ba80d679df2326ce37b7a2e277509
Андрей Анатольев <a09012018@mail.ru>
2023-05-05 10:48:56 +0500

    removed print from the module

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -4,15 +4,14 @@ rules_of_game = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def body_of_games():
-    number = randint(1, 100)
-    print('Question:', number)
-    print('Your answer: ', end='')
-    answer_user = input()
+    task = randint(1, 100)
     answer_correct = 'yes'
     count = 0
-    for i in range(1, number + 1):
-        if number % i == 0:
+
+    for i in range(1, task + 1):
+        if task % i == 0:
             count += 1
+
     if count > 2:
         answer_correct = 'no'
-    return answer_correct, answer_user
+    return task, answer_correct
