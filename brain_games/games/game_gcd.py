d8d86ce7bc9d367eed87735a177de5d0e5a230df
Андрей Анатольев <a09012018@mail.ru>
2023-05-05 10:48:23 +0500

    removed print from the module

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -7,8 +7,6 @@ rules_of_game = 'Find the greatest common divisor of given numbers.'
 def body_of_games():
     n = randint(1, 100)
     m = randint(1, 100)
-    print('Question:', n, m)
-    print('Your answer: ', end='')
-    answer_user = input()
+    task = f'{n} {m}'
     answer_correct = str(gcd(n, m))
-    return answer_correct, answer_user
+    return task, answer_correct
