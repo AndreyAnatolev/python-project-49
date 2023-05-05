4bd08e153ae54acf0f0e24827f0f36c7357af39a
Андрей Анатольев <a09012018@mail.ru>
2023-05-05 10:47:26 +0500

    brain_games/games/game_even.py

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -4,12 +4,9 @@ rules_of_game = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def body_of_games():
-    n = randint(1, 99)
-    print('Question:', n)
-    print('Your answer: ', end='')
-    answer_user = input()
-    if n % 2 == 0:
+    task = randint(1, 99)
+    if task % 2 == 0:
         answer_correct = 'yes'
     else:
         answer_correct = 'no'
-    return answer_correct, answer_user
+    return task, answer_correct
