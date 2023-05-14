2820024caaf74966a09630fdb144343c39c858ba
Андрей Анатольев <a09012018@mail.ru>
2023-05-14 10:41:23 +0500

    added def even_numbered()

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,11 +1,18 @@
 from random import randint
 
-rules_of_game = 'Answer "yes" if the number is even, otherwise answer "no".'
+RULES_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def even_numbered(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 
 def body_of_games():
     task = randint(1, 99)
-    if task % 2 == 0:
+    if even_numbered(task) is True:
         answer_correct = 'yes'
     else:
         answer_correct = 'no'
