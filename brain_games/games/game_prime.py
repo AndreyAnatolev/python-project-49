61ca7e62f130a5dbf1936b7af4369f119e3fd64e
Андрей Анатольев <a09012018@mail.ru>
2023-05-14 10:42:31 +0500

    added def simple_number()

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -1,17 +1,23 @@
 from random import randint
 
-rules_of_game = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+RULES_OF_GAME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def simple_number(number):
+    count = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            count += 1
+    if count == 2:
+        return True
+    else:
+        return False
 
 
 def body_of_games():
     task = randint(1, 100)
-    answer_correct = 'yes'
-    count = 0
-
-    for i in range(1, task + 1):
-        if task % i == 0:
-            count += 1
-
-    if count > 2:
+    if simple_number(task) is True:
+        answer_correct = 'yes'
+    else:
         answer_correct = 'no'
     return task, answer_correct
