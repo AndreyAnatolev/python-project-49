a56c3081fb26ba13c42fdbc74a59d9dcfb06a74e
Андрей Анатольев <a09012018@mail.ru>
2023-05-14 19:07:59 +0500

    added const, rename func

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -2,11 +2,14 @@ from random import randint
 from math import gcd
 
 RULES_OF_GAME = 'Find the greatest common divisor of given numbers.'
+MIN_VALUE_FOR_RANDOM = 1
+MAX_VALUE_FOR_RANDOM = 100
 
 
-def body_of_games():
-    n = randint(1, 100)
-    m = randint(1, 100)
-    task = f'{n} {m}'
-    answer_correct = str(gcd(n, m))
+def task_and_answer_correct():
+    first_random_number = randint(MIN_VALUE_FOR_RANDOM, MAX_VALUE_FOR_RANDOM)
+    second_random_number = randint(MIN_VALUE_FOR_RANDOM, MAX_VALUE_FOR_RANDOM)
+    task = f'{first_random_number} {second_random_number}'
+    answer_correct = str(gcd(first_random_number, second_random_number))
+
     return task, answer_correct
