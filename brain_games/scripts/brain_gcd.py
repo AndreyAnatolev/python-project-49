3820b83f9f152af3cff9a2838b9cde86b3eda4dd
Андрей Анатольев <a09012018@mail.ru>
2023-05-14 18:38:36 +0500

    engine.run(games)

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games import logic_of_games
+from brain_games import logic_of_games as engine
 from brain_games.games import game_gcd
 
 
 def main():
-    logic_of_games.logic_games(game_gcd)
+    engine.run(game_gcd)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games import logic_of_games
+from brain_games import logic_of_games as engine
 from brain_games.games import game_prime
 
 
 def main():
-    logic_of_games.logic_games(game_prime)
+    engine.run(game_prime)
 
 
 if __name__ == '__main__':
