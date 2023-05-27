279e6f3b0e69cb005e8c41ef660f6bb426ed3812
Андрей Анатольев <a09012018@mail.ru>
2023-05-27 20:52:15 +0500

    rename argument in def run(game)

--- brain_games/logic_of_games.py
+++ brain_games/logic_of_games.py
@@ -3,15 +3,15 @@ import prompt
 NUMBER_OF_ROUNDS = 3
 
 
-def run(game_variants):
+def run(game):
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
-    print(game_variants.RULES_OF_GAME)
+    print(game.RULES_OF_GAME)
 
     count = 0
     for _ in range(NUMBER_OF_ROUNDS):
-        task, answer_correct = game_variants.task_and_answer_correct()
+        task, answer_correct = game.task_and_answer_correct()
         print('Question:', task)
         answer_user = input('Your_answer: ')
 
