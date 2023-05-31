96de31981a9c718f224dfa8b1f5c01dfe7e5c335
Андрей Анатольев <a09012018@mail.ru>
2023-05-31 11:18:40 +0500

    overwriting

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -5,18 +5,18 @@ RULES_OF_GAME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_the_number_prime(number):
-    if number != 1:
-        count = 0
-        for i in range(2, int(sqrt(number))):
-            if number % i == 0:
-                count += 1
-                break
-        return count == 0
-    else:
+    if number in (0, 1):
         return False
+
+    for i in range(2, int(sqrt(number))):
+        if number % i == 0:
+            return False
+
+    return True
 
 
 def create_the_task():
     task = randint(1, 100)
     answer_correct = ('no', 'yes')[is_the_number_prime(task)]
     return task, answer_correct
+
